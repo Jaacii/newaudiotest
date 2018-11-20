10d5b615409da52ef10462d0cc83267dc0f80b0b
Jaacii <sliphno@hotmail.de>
2018-11-20 13:24:26 +0100

    test

---
+++ JS/Volume.js
@@ -0,0 +1,13 @@
+//Volume Slider
+
+	var  volumeslider
+	
+	
+		volumeslider = document.getElementById("volumeslider");
+		volumeslider.addEventListener("mousemove", setvolume);
+		
+		
+		
+	function setvolume(){
+	    audioElement2.volume = volumeslider.value / 100;
+		}
