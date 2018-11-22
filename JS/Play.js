b542d2462f58a8413963644e21ebb9088790013e
Jaacii <sliphno@hotmail.de>
2018-11-22 13:01:25 +0100

    test

--- JS/Play.js
+++ JS/Play.js
@@ -1,9 +1,8 @@
  
    //------------Play Button
 		var onOff = 0;
-		var btn = document.querySelector('.playbtn');
-	
-			/*	button.onclick = function () {*/function playOrPauseSong() {
+		
+			function playOrPauseSong() {
 				
 				
 				
@@ -13,16 +12,14 @@
 					audioElement2.play();
 				
 					//Pic change
-					document.getElementById("playbtn").style.background ='url (https://svgsilh.com/svg_v2/27429.svg)';
-					//document.getElementById("playbtn").src="pause.png";
+					document.getElementById("playpic").src="../pause.png";
 					onOff = 1;
 				}
 				else {
 					
 					audioElement2.pause();     			  
 					//Pic change
-					document.getElementById("playbtn").style.background ='url (https://svgsilh.com/svg_v2/1718976.svg)';
-					//document.getElementById("playbtn").src="play.png";
+					document.getElementById("playpic").src="../play.png";
 					onOff = 0;
 				}
 			}
