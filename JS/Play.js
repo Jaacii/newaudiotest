f59e0f345e90bb64d3f0c79f4296f13bd7974d58
Jaacii <sliphno@hotmail.de>
2018-11-22 13:10:11 +0100

    test

--- JS/Play.js
+++ JS/Play.js
@@ -1,7 +1,21 @@
  
    //------------Play Button
 		var onOff = 0;
+		var button = document.querySelector('.button');
 		
+		button.onclick = function () {
+			if (onOff == 0) {
+				this.style.background = "url (https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Pause-thin-rounded-button.svg/2000px-Pause-thin-rounded-button.svg.png)";
+				counter = 1;
+			}
+			else if () {
+				this.style.background = "url (https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Play-rounded-button-outline.svg/2000px-Play-rounded-button-outline.svg.png)";
+				counter = 0;
+			}
+		};
+		
+		
+		/*
 			function playOrPauseSong() {
 				
 				
@@ -22,4 +36,4 @@
 					document.getElementById("playpic").src="play.png";
 					onOff = 0;
 				}
-			}
+			}*/
